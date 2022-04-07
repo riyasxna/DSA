2f8ee186ee121bfaf08f946e6ac9619d2b654d89
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-04-08 00:13:19 +0530

    Time: 291 ms (18.89%), Space: 88.9 MB (39.62%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n=temp.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && temp[i]>temp[st.top()]){
+                ans[st.top()]=i-st.top();
+                st.pop();
+            }
+                st.push(i);
+        }
+        return ans;
+    }
+};
