435ebf87bc29c889e7bc7ed568a86145418744b3
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-04-15 03:05:29 +0530

    Time: 172 ms (89.26%), Space: 60.7 MB (94.02%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        int count[n+1];
+        for(int i=1;i<n+1;i++){
+            count[i]=0;
+        }
+        for(int i=0;i<trust.size();i++){
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
+        }
+        for(int i=1;i<n+1;i++){
+            if(count[i]==n-1)
+                return i;
+        }
+        return -1;
+    }
+};
