a2a7b95f64486c70088196f708787620297380c4
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-05-09 01:57:16 +0530

    Added solution - LeetHub

---
+++ Buy Maximum Stocks if i stocks can be bought on i-th day - GFG/buy-maximum-stocks-if-i-stocks-can-be-bought-on-ith-day.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+    int buyMaximumProducts(int n, int k, int price[]){
+        //Write your code here
+        int ans=0;
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++){
+            v.push_back({price[i],i+1});
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i++){
+            ans+=min(v[i].second,k/v[i].first);
+            k-=v[i].first*min(v[i].second,k/v[i].first);
+        }
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin >> n >> k;
+	    int price[n];
+	    for(int i = 0 ; i < n; i++){
+	        cin >> price[i];
+	    }
+	    Solution ob;
+	    int ans = ob.buyMaximumProducts(n, k, price);
+	    cout << ans<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
