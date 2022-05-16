7ad46f906564465fffc49b6b432404f67ac7840b
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-05-17 02:18:12 +0530

    Time: 169 ms (53.98%), Space: 74.4 MB (74.00%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long index=0,count=0,n=chalk.size();
+        if(n==1)
+            return 0;
+        for(int i=0;i<chalk.size();i++){
+            count+=chalk[i];
+        }
+        k%=count;
+        if(k==0)
+            return 0;
+        for(index=0;index<n;index++){
+            if(chalk[index]<=k){
+                k-=chalk[index];
+            }
+            else
+                break;
+        }
+        return index;
+    }
+};
