4e8f4732ce437846253110a70c65e415018bdc72
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-06-25 01:06:28 +0530

    Time: 93 ms (21.59%), Space: 18.9 MB (25.72%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(),piles.end());
+        long long l=1,r=piles[piles.size()-1],mid,k=LLONG_MAX;
+        while(l<=r){
+            mid=(l+r)/2;
+            long long i=0,count=0;
+            while(i<piles.size()){
+                if(piles[i]<=mid){
+                    count++;
+                    i++;
+                }
+                else{
+                    count+=piles[i]/mid;
+                    if(piles[i]%mid!=0)
+                        count++;
+                    i++;
+                }
+            }
+            if(count<=h){
+                k=min(k,mid);
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return k;
+    }
+};
