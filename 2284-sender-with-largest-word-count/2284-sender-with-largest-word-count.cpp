09d10bd38963723703fccb19ee8121f0c9fd2e96
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-06-29 01:40:30 +0530

    Time: 225 ms (95.78%), Space: 72 MB (64.09%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     string largestWordCount(vector<string>& messages, vector<string>& senders) {
-        map<string,int> um;
-        string res;
+        unordered_map<string,int> um;
+        string res="";
         int value=-1;
         for(int i=0;i<messages.size();i++){
             um[senders[i]]+=count(begin(messages[i]), end(messages[i]), ' ')+1;
         }
         for(auto it:um){
-            if(it.second>=value){
+            if(it.second>value){
                 value=it.second;
                 res=it.first;
             }
+            else if(it.second==value && it.first>res)
+                res=it.first;
         }
         return res;
     }
