995a166f1aaa5c5fea4d0003b2fe0a4048a3793e
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-06-30 01:00:41 +0530

    Time: 19 ms (50.28%), Space: 10.5 MB (42.68%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.size(),start=0,len=0,temp;
+        int n=s.size();
         int res=0;
         int dp[n][n];
         memset(dp,0,sizeof(dp));
@@ -9,16 +9,10 @@ public:
             dp[i][i]=1;
             res++;
         }
-        len=1;
         for(int i=0;i<n-1;i++){
             if(s[i]==s[i+1]){
                 dp[i][i+1]=1;
                 res++;
-                temp=2;
-                if(temp>len){
-                    start =i;
-                    len=2;
-                }
             }
         }
         for(int k=3;k<=n;k++){
@@ -27,11 +21,7 @@ public:
                 if(s[i]==s[j] && dp[i+1][j-1]==1){
                     dp[i][j]=1;
                     res++;
-                    if(k>len){
-                        len=k;
-                        start=i;
                     }
-                }
             }
         }
         return res;
