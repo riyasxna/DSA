c8ec5790cc6d042c95c543ad71260e4139c92053
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-06-30 02:07:24 +0530

    Added solution - LeetHub

---
+++ Convert a sentence into its equivalent mobile numeric keypad sequence - GFG/convert-a-sentence-into-its-equivalent-mobile-numeric-keypad-sequence.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// C++ implementation to convert a
+// sentence into its equivalent
+// mobile numeric keypad sequence
+#include <bits/stdc++.h>
+using namespace std;
+string printSequence(string input);
+// Driver function
+int main()
+{
+	int t;
+	cin>>t;
+	cin.ignore();
+	while(t--)
+    {
+        string s;
+        getline(cin,s);
+        cout<<printSequence(s)<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+string printSequence(string S)
+{
+    //code here.
+    string arr[]={ "2","22","222",
+                   "3","33","333",
+                   "4","44","444",
+                   "5","55","555",
+                   "6","66","666",
+                   "7","77","777","7777",
+                   "8","88","888",
+                   "9","99","999","9999"
+       
+   };
+   string output="";
+   for(int i=0; i<S.length(); i++){
+       if(S[i]==' '){
+           output= output+'0';
+       }
+       else{
+           int position= S[i]-'A';
+           output= output + arr[position];
+       }
+   }
+   return output;
+}
