56f53010c0603ac646f7d72f1e6a29022783b43b
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-07-15 03:18:49 +0530

    Time: 39 ms (7.54%), Space: 17.8 MB (40.87%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>0 || st.empty() || asteroids[i]<0 && st.top()<0)
+                st.push(asteroids[i]);
+            else if(asteroids[i]<0 && !st.empty()){
+                while(!asteroids.empty() && st.top()>0){
+                    if(abs(st.top())==abs(asteroids[i])){
+                        st.pop();
+                        break;
+                    }   
+                    else if(abs(st.top())>abs(asteroids[i])){
+                        break;
+                    }
+                    else{
+                        st.pop();
+                        if(st.empty() || st.top()<0)
+                            st.push(asteroids[i]);
+                    }
+                }
+            }
+        }
+        vector<int> res(st.size());
+        for(int i=res.size()-1;i>=0;i--){
+            res[i]=st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
