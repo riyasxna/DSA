e9769004aad1db9b105ba6a5282e3aa2a83f4e72
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-07-26 14:30:49 +0530

    Time: 4 ms (31.53%), Space: 6.2 MB (49.67%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1,1);
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                dp[i]=max({dp[i],j*dp[i-j],j*(i-j)});
+            }
+        }
+        return dp[n];
+    }
+};
