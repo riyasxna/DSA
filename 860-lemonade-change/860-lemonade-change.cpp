115ea3bb7a7e036cab5b2c645788410f8c6edc71
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-08-22 03:39:07 +0530

    Time: 127 ms (83.83%), Space: 83.3 MB (97.02%) - LeetHub

---
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0,twenty=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5)
+                five++;
+            else if(bills[i]==10){
+                if(five==0)
+                    return false;
+                ten++;
+                five--;
+            }
+            else{
+                if((ten>=1 && five>=1)){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3)
+                    five-=3;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
