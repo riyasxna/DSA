d305428854204bdea2baf400655bfff1e3591605
Riya Saxena <88880316+riyasxna@users.noreply.github.com>
2022-08-21 21:51:31 +0530

    Time: 8 ms (92.02%), Space: 12.2 MB (36.73%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    vector<int> res;
+    void recurse(int m,int n){
+        if(m>n)
+            return;
+        res.push_back(m);
+        recurse(m*10,n);
+        if(m%10!=9)
+            recurse(m+1,n);
+    }
     vector<int> lexicalOrder(int n) {
-        vector<int> res;
-        vector<string> temp;
-        for(int i=1;i<=n;i++){
-            temp.push_back(to_string(i));
-        }
-        sort(temp.begin(),temp.end());
-        for(int i=0;i<temp.size();i++){
-            res.push_back(stoi(temp[i]));
-        }
+        recurse(1,n);
         return res;
     }
 };
